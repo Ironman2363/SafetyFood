fe49284566151e0ce1580b4fd9116d5e9b691cda
hung0920 <hungooso4413@gmail.com>
2022-11-14 08:03:29 +0700

    Splash_Screen-to-Login_Screen

--- app/src/main/java/com/example/safetyfood/Activities/Splash_Screen.java
+++ app/src/main/java/com/example/safetyfood/Activities/Splash_Screen.java
@@ -2,7 +2,9 @@ package com.example.safetyfood.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 import com.example.safetyfood.R;
 
@@ -12,5 +14,18 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        CountDownTimer countDownTimer = new CountDownTimer(3000,3000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent( getApplicationContext(),Login.class ));
+            }
+        };
+        countDownTimer.start();
     }
 }
