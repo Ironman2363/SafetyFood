404a58c5384456d3afdd70a8784c5ce73dac98cb
nxc2k3 <cuongnxph20131@fpt.edu.vn>
2022-11-15 17:09:12 +0700

    password change

---
+++ app/src/main/java/com/example/safetyfood/Activities/Passwordchange.java
@@ -0,0 +1,30 @@
+package com.example.safetyfood.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.safetyfood.R;
+
+public class Passwordchange extends AppCompatActivity {
+    EditText edtOldPass = findViewById(R.id.edtOldPass);
+    EditText edtNewPass = findViewById(R.id.edtNewPass);
+    EditText edtReNewPass = findViewById(R.id.edtReNewPass);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passwordchange);
+
+        String oldPass = edtOldPass.getText().toString();
+        String newPass = edtNewPass.getText().toString();
+        String renewPass = edtReNewPass.getText().toString();
+        if (oldPass.equals("") || newPass.equals("") || renewPass.equals("")) {
+            Toast.makeText(Passwordchange.this, "Phải nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
+    }
+}
