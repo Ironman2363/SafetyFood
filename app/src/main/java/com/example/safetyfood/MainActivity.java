266e0b1a204164c9ca69e1a7edd030bb4125e63a
DucSenSei <phamxuanduc25703@gmail.com>
2022-11-16 08:50:22 +0700

    Duc_trinh_sua_bottom_menu
    ok

--- app/src/main/java/com/example/safetyfood/MainActivity.java
+++ app/src/main/java/com/example/safetyfood/MainActivity.java
@@ -1,26 +1,30 @@
 package com.example.safetyfood;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Bundle;
 
 import com.example.safetyfood.FRAGMENT.CartFragment;
 import com.example.safetyfood.FRAGMENT.HomeFragment;
 import com.example.safetyfood.FRAGMENT.OrderFragment;
 import com.example.safetyfood.FRAGMENT.SettingFragment;
 import com.example.safetyfood.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()){
@@ -37,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.setting:
                     replaceFragment(new SettingFragment());
                     break;
-
             }
 
 
