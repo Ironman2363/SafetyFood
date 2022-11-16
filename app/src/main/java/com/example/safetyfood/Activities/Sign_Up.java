1419288abe24d82a54f81d2b0cd6d0557350156c
hung0920 <hungooso4413@gmail.com>
2022-11-16 21:01:14 +0700

    Lee_Hung_Sign_Up

--- app/src/main/java/com/example/safetyfood/Activities/Sign_Up.java
+++ app/src/main/java/com/example/safetyfood/Activities/Sign_Up.java
@@ -1,16 +1,56 @@
 package com.example.safetyfood.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.safetyfood.R;
 
 public class Sign_Up extends AppCompatActivity {
 
+    Toolbar toolbar;
+    TextView txt_toLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        anhXa();
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(null);
+
+        txt_toLogin.setOnClickListener(v -> {
+            toLoginListener();
+        });
+    }
+
+    private void toLoginListener() {
+        startActivity(new Intent(this,Login.class));
+    }
+
+    private void anhXa() {
+        toolbar = findViewById(R.id.SU_tool_bar);
+        txt_toLogin = findViewById(R.id.txt_toLogin);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                onBackPressed();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
