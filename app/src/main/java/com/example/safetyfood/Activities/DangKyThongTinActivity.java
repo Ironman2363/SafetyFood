741df4cc6904227bc6c338d899fa5adef119041b
Phạm Tiến Dũng <phamtiendungvn2363@gmail.com>
2022-11-23 07:46:10 +0700

    admin

--- app/src/main/java/com/example/safetyfood/Activities/DangKyThongTinActivity.java
+++ app/src/main/java/com/example/safetyfood/Activities/DangKyThongTinActivity.java
@@ -2,15 +2,29 @@ package com.example.safetyfood.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.safetyfood.MainActivity;
 import com.example.safetyfood.R;
 
 public class DangKyThongTinActivity extends AppCompatActivity {
 
+    Button hoantat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky_thong_tin);
+        hoantat = findViewById(R.id.btnDangkythongtin);
+        hoantat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DangKyThongTinActivity.this, MainActivity.class));
+                Toast.makeText(DangKyThongTinActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
