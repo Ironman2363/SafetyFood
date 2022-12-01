184cfe85f16a50e5db48499d0344dea0347acb96
hung0920 <hungooso4413@gmail.com>
2022-12-01 19:14:11 +0700

    LHung:Order_History

---
+++ app/src/main/java/com/example/safetyfood/ADAPTER/OrderHistoryAdapter.java
@@ -0,0 +1,95 @@
+package com.example.safetyfood.ADAPTER;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.safetyfood.DAO.ChiTietDatHangDAO;
+import com.example.safetyfood.MODEL.DatHang;
+import com.example.safetyfood.R;
+
+import java.util.List;
+
+public class OrderHistoryAdapter extends RecyclerView.Adapter<OrderHistoryAdapter.OrderHistoryAdapterHolder>{
+
+    List<DatHang> list;
+    Context context;
+    ChiTietDatHangDAO chiTietDatHangDAO;
+
+    public OrderHistoryAdapter(List<DatHang> list, Context context) {
+        this.list = list;
+        this.context = context;
+        chiTietDatHangDAO = new ChiTietDatHangDAO(context);
+    }
+
+    @NonNull
+    @Override
+    public OrderHistoryAdapterHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new OrderHistoryAdapterHolder(LayoutInflater.from(parent.getContext( )).inflate(R.layout.order_history_items,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderHistoryAdapterHolder holder, int position) {
+        DatHang datHang = list.get(position);
+        Log.e("Adapter", "onBindViewHolder: "+datHang );
+        String status = "";
+        holder.Order_History_items_ID.setText("Mã đơn :"+datHang.getId());
+        holder.Order_History_items_Buyer.setText("Khách hàng : Hung");
+        holder.Order_History_items_Address.setText("Địa chỉ : HN");
+        holder.Order_History_items_Amount.setText(chiTietDatHangDAO.getSum(datHang.getId( ))+" sản phẩm");
+        holder.Order_History_items_TotalPrice.setText("Tổng :"+datHang.getTotalpriceDathang()+"VND");
+        switch (datHang.getStatusDathang()){
+            case 1:{
+                status = "Đang chờ xử lý";
+                holder.Order_History_items_Status.setTextColor(Color.GREEN);
+                break;
+            }
+            case 2:{
+                status = "Đang giao";
+                holder.Order_History_items_Status.setTextColor(Color.GREEN);
+                break;
+            }
+            case 3:{
+            }
+            case 4:{
+                status = "Đã hủy";
+                holder.Order_History_items_Status.setTextColor(Color.RED);
+                break;
+            }
+            case 5:{
+                status = "Đã nhận";
+                holder.Order_History_items_Status.setTextColor(Color.GREEN);
+                break;
+            }
+        }
+        holder.Order_History_items_Status.setText("Trạng thái :"+status);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class OrderHistoryAdapterHolder extends RecyclerView.ViewHolder {
+
+        TextView Order_History_items_ID,Order_History_items_Buyer,Order_History_items_Address,Order_History_items_Amount,
+                Order_History_items_TotalPrice,Order_History_items_Status;
+
+        public OrderHistoryAdapterHolder(@NonNull View itemView) {
+            super(itemView);
+            Order_History_items_ID = itemView.findViewById(R.id.Order_History_items_ID);
+            Order_History_items_Buyer = itemView.findViewById(R.id.Order_History_items_Buyer);
+            Order_History_items_Address = itemView.findViewById(R.id.Order_History_items_Address);
+            Order_History_items_Amount = itemView.findViewById(R.id.Order_History_items_Amount);
+            Order_History_items_TotalPrice = itemView.findViewById(R.id.Order_History_items_TotalPrice);
+            Order_History_items_Status = itemView.findViewById(R.id.Order_History_items_Status);
+        }
+    }
+}
--- app/src/main/java/com/example/safetyfood/Admin/SeingAdminFragment.java
+++ app/src/main/java/com/example/safetyfood/Admin/SeingAdminFragment.java
@@ -3,33 +3,53 @@ package com.example.safetyfood.Admin;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.safetyfood.ADAPTER.OrderHistoryAdapter;
+import com.example.safetyfood.DAO.DatHangDAO;
+import com.example.safetyfood.MODEL.DatHang;
 import com.example.safetyfood.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SeingAdminFragment extends Fragment {
 
-    public SeingAdminFragment() {
-    }
-
-    public static SeingAdminFragment newInstance() {
-        SeingAdminFragment fragment = new SeingAdminFragment();
-
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    RecyclerView Order_History_List;
+    View view;
+    List<DatHang> list;
+    DatHangDAO datHangDAO;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_seing_admin, container, false);
+
+        view = inflater.inflate(R.layout.fragment_seing_admin, container, false);
+
+        Order_History_List = view.findViewById(R.id.Order_History_List);
+        datHangDAO = new DatHangDAO(view.getContext());
+
+        getData();
+
+        return view;
+    }
+
+    private void getData() {
+        list = new ArrayList<>(  );
+        list = datHangDAO.getOrderHistory(0);
+        setData();
+    }
+
+    private void setData() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        Order_History_List.setLayoutManager(linearLayoutManager);
+        OrderHistoryAdapter adapter = new OrderHistoryAdapter(list,getContext());
+        Order_History_List.setAdapter(adapter);
     }
 }
