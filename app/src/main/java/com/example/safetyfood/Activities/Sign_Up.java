90eeaca26d51ecf8b1f6bc803e281ae195f3c568
nxc2k3 <cuongnxph20131@fpt.edu.vn>
2022-12-01 22:20:47 +0700

    DangKyThongTinKhachHang

--- app/src/main/java/com/example/safetyfood/Activities/Sign_Up.java
+++ app/src/main/java/com/example/safetyfood/Activities/Sign_Up.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -12,16 +13,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import com.example.safetyfood.DAO.TaikhoanDAO;
+import com.example.safetyfood.MODEL.TaiKhoan;
 import com.example.safetyfood.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Sign_Up extends AppCompatActivity {
+    TextInputEditText SU_edt_UserName;
+    TextInputEditText SU_edt_PassWord;
+    TextInputEditText SU_edt_RePassWord;
+    TaikhoanDAO taikhoanDAO;
 
     Toolbar toolbar;
     TextView txt_toLogin;
@@ -45,10 +46,26 @@ public class Sign_Up extends AppCompatActivity {
         appCompatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Sign_Up.this, DangKyThongTinActivity.class));
+
+                String username = SU_edt_UserName.getText().toString();
+                String password = SU_edt_PassWord.getText().toString();
+                String repass = SU_edt_RePassWord.getText().toString();
+
+                DangKy(username, password, repass);
             }
         });
 
+    }
+
+    private void DangKy(String username, String password, String repass) {
+        if (password.equals(repass)){
+            TaiKhoan taiKhoan = new TaiKhoan(0, username, password, 2);
+            taikhoanDAO.insertTaikhoan(taiKhoan);
+            startActivity(new Intent(Sign_Up.this, DangKyThongTinActivity.class));
+        }
+        else{
+            Toast.makeText(this, "Hai mật khẩu chưa trùng nhau", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void toLoginListener() {
@@ -59,6 +76,11 @@ public class Sign_Up extends AppCompatActivity {
         toolbar = findViewById(R.id.SU_tool_bar);
         txt_toLogin = findViewById(R.id.txt_toLogin);
         appCompatButton = findViewById(R.id.appCompatButton);
+
+        SU_edt_UserName = findViewById(R.id.SU_edt_UserName);
+        SU_edt_PassWord = findViewById(R.id.SU_edt_PassWord);
+        SU_edt_RePassWord = findViewById(R.id.SU_edt_RePassWord);
+        taikhoanDAO = new TaikhoanDAO(this);
     }
 
     @Override
