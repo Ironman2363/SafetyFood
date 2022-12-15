95f5ee2fbc9aaa48c36bf88d4306fd16ebe078e7
hung0920 <hungooso4413@gmail.com>
2022-12-15 21:17:36 +0700

    Update_Sua_Admin

---
+++ app/src/main/java/com/example/safetyfood/Activities/getImg.java
@@ -0,0 +1,113 @@
+package com.example.safetyfood.Activities;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.safetyfood.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class getImg extends AppCompatActivity {
+
+    String link;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_img);
+
+        Intent intent = getIntent();
+        int type = intent.getIntExtra("type",-1);
+        switch (type){
+            case 0 :{
+                openImage();
+                break;
+            }
+            case 1 :{
+                imageChoose();
+                break;
+            }
+        }
+
+    }
+
+    private void openImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(this.getPackageManager( )) != null) {
+            activityResultLauncher.launch(intent);
+        } else {
+            Toast.makeText(getApplicationContext( ), "app ko ho tro action", Toast.LENGTH_SHORT).show( );
+        }
+    }
+
+    private void imageChoose() {
+        Intent i = new Intent( );
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        LauncherSomeActivity.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> LauncherSomeActivity =
+            registerForActivityResult(new ActivityResultContracts
+                            .StartActivityForResult( ), result -> {
+                        if (result.getResultCode( ) == RESULT_OK) {
+                            Intent data = result.getData( );
+                            if (data != null && data.getData( ) != null) {
+                                String path = null;
+                                Uri selectImageUri = data.getData( );
+                                Bitmap SeclectImageBitmap = null;
+                                try {
+                                    SeclectImageBitmap = MediaStore.Images.Media.getBitmap(getApplicationContext( ).getContentResolver( ), selectImageUri);
+
+                                } catch (IOException e) {
+                                    e.printStackTrace( );
+                                }
+                                link = String.valueOf(getImageUri(getApplicationContext(),SeclectImageBitmap));
+                                Intent sendBack = new Intent(  );
+                                sendBack.setAction("sendBack");
+                                sendBack.putExtra("link",link);
+                                LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(sendBack);
+                                finish();
+//                               Log.e("zzz",link);
+                            }
+                        }
+                    }
+            );
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult( ), new ActivityResultCallback<ActivityResult>( ) {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode( ) == RESULT_OK && result.getData( ) != null) {
+                Bundle bundle = result.getData( ).getExtras( );
+                Bitmap bitmap = (Bitmap) bundle.get("data");
+                link = String.valueOf(getImageUri(getApplicationContext(),bitmap));
+                Intent sendBack = new Intent(  );
+                sendBack.setAction("sendBack");
+                sendBack.putExtra("link",link);
+                LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(sendBack);
+                finish();
+            }
+        }
+    });
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream( );
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver( ), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+}
--- app/src/main/java/com/example/safetyfood/Tab_Fragment/SanPhamFragment.java
+++ app/src/main/java/com/example/safetyfood/Tab_Fragment/SanPhamFragment.java
@@ -5,9 +5,11 @@ import static android.app.Activity.RESULT_OK;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResult;
@@ -22,6 +24,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -47,6 +51,7 @@ import com.example.safetyfood.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -57,7 +62,7 @@ public class SanPhamFragment extends Fragment {
     RecyclerView list_sp;
     SanPham sanPham;
     ThemSanPhamAdapter phamAdapter;
-    List<SanPham> list = new ArrayList<>();
+    List<SanPham> list = new ArrayList<>( );
     List<LoaiSanPham> list_loai_sp;
     LoaiSanPhamDAO phamDAO;
     SanPhamDAO sanPhamDAO;
@@ -74,36 +79,70 @@ public class SanPhamFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_san_pham, container, false);
         add_sp = view.findViewById(R.id.add_sp);
         list_sp = view.findViewById(R.id.list_sp);
-        sanPhamDAO = new SanPhamDAO(getContext());
+        sanPhamDAO = new SanPhamDAO(getContext( ));
 //        add();
-        RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext());
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(getContext( ));
         list_sp.setLayoutManager(manager);
-        list = sanPhamDAO.getDSSanPham();
-        phamAdapter = new ThemSanPhamAdapter(getContext(), list);
+        list = sanPhamDAO.getDSSanPham( );
+        phamAdapter = new ThemSanPhamAdapter(getContext( ), list);
         list_sp.setAdapter(phamAdapter);
         registerForContextMenu(list_sp);
-        add_sp.setOnClickListener(new View.OnClickListener() {
+        add_sp.setOnClickListener(new View.OnClickListener( ) {
             @Override
             public void onClick(View v) {
-                themSanPham();
+                themSanPham( );
             }
         });
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult( ), new ActivityResultCallback<ActivityResult>( ) {
             @Override
             public void onActivityResult(ActivityResult result) {
-                if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                    Bundle bundle = result.getData().getExtras();
+                if (result.getResultCode( ) == RESULT_OK && result.getData( ) != null) {
+                    Bundle bundle = result.getData( ).getExtras( );
                     Bitmap bitmap = (Bitmap) bundle.get("data");
                     if (bitmap != null) {
                         anh.setImageBitmap(bitmap);
                         anh.setVisibility(View.VISIBLE);
                     }
-                    link = String.valueOf(getImageUri(getContext(), bitmap));
+                    link = String.valueOf(getImageUri(getContext(),bitmap));
                 }
             }
         });
         return view;
     }
+
+    private void imageChoose() {
+        Intent i = new Intent( );
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        LauncherSomeActivity.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> LauncherSomeActivity =
+            registerForActivityResult(new ActivityResultContracts
+                            .StartActivityForResult( ), result -> {
+                        if (result.getResultCode( ) == RESULT_OK) {
+                            Intent data = result.getData( );
+                            if (data != null && data.getData( ) != null) {
+                                String path = null;
+                                Uri selectImageUri = data.getData( );
+                                Bitmap SeclectImageBitmap = null;
+                                try {
+                                    SeclectImageBitmap = MediaStore.Images.Media.getBitmap(getContext( ).getContentResolver( ), selectImageUri);
+
+                                } catch (IOException e) {
+                                    e.printStackTrace( );
+                                }
+
+                                if (SeclectImageBitmap != null) {
+                                    anh.setImageBitmap(SeclectImageBitmap);
+                                    anh.setVisibility(View.VISIBLE);
+                                }
+                                link = String.valueOf(getImageUri(getContext(),SeclectImageBitmap));
+//                               Log.e("zzz",link);
+                            }
+                        }
+                    }
+            );
 
     //    private void add() {
 //        list.add(new SanPham("pepsi",String.valueOf(R.drawable.pepsi),10000,1,"20/22/2022"));
@@ -113,19 +152,19 @@ public class SanPhamFragment extends Fragment {
 //        list.add(new SanPham("pepsi",String.valueOf(R.drawable.pepsi),400000,3,"20/22/2022"));
 //
 //    }
-    public static Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream( );
         inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "IMG_" + Calendar.getInstance().getTime(),null);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver( ), inImage, "Title", null);
         return Uri.parse(path);
     }
 
     private void themSanPham() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_san_pham, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext( ));
+        View view = LayoutInflater.from(getContext( )).inflate(R.layout.dialog_san_pham, null);
         builder.setView(view);
-        Dialog dialog = builder.create();
-        dialog.show();
+        Dialog dialog = builder.create( );
+        dialog.show( );
         LinearLayout them_anh = view.findViewById(R.id.them_anh);
         Spinner loai_sp = view.findViewById(R.id.spinner_loai_sp);
         EditText ten_sp = view.findViewById(R.id.edit_ten_sp);
@@ -133,23 +172,36 @@ public class SanPhamFragment extends Fragment {
         Button them = view.findViewById(R.id.btn_add_sp);
         anh = view.findViewById(R.id.img_loai_sp);
 
-        list_loai_sp = new ArrayList<>();
-        phamDAO = new LoaiSanPhamDAO(getContext());
+        list_loai_sp = new ArrayList<>( );
+        phamDAO = new LoaiSanPhamDAO(getContext( ));
 
-        them_anh.setOnClickListener(new View.OnClickListener() {
+        them_anh.setOnClickListener(new View.OnClickListener( ) {
             @Override
             public void onClick(View v) {
-                openImage();
+                final CharSequence[] items = {"Chụp ảnh", "Chọn ảnh từ thư viện"};
+                android.app.AlertDialog.Builder builder1 = new android.app.AlertDialog.Builder(getContext( ));
+                builder1.setIcon(R.drawable.img_image);
+                builder1.setItems(items, new DialogInterface.OnClickListener( ) {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (items[which].equals("Chụp ảnh")) {
+                            openImage( );
+                        } else if (items[which].equals("Chọn ảnh từ thư viện")) {
+                            imageChoose( );
+                        }
+                    }
+                });
+                builder1.show( );
             }
         });
-        list_loai_sp = phamDAO.getDSLoaiSanPham();
-        spinnerAdapter = new LoaiSanPhamSpinnerAdapter(getContext(), list_loai_sp);
+        list_loai_sp = phamDAO.getDSLoaiSanPham( );
+        spinnerAdapter = new LoaiSanPhamSpinnerAdapter(getContext( ), list_loai_sp);
         loai_sp.setAdapter(spinnerAdapter);
 
-        loai_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        loai_sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener( ) {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                maLoaiSanPham = list_loai_sp.get(position).getId();
+                maLoaiSanPham = list_loai_sp.get(position).getId( );
             }
 
             @Override
@@ -167,30 +219,30 @@ public class SanPhamFragment extends Fragment {
 //        }
 //        loai_sp.setSelection(position);
 
-        them.setOnClickListener(new View.OnClickListener() {
+        them.setOnClickListener(new View.OnClickListener( ) {
             @Override
             public void onClick(View v) {
-                sanPham = new SanPham();
-                String name = ten_sp.getText().toString();
-                String price = gia_sp.getText().toString();
-                String image = anh.getResources().toString();
+                sanPham = new SanPham( );
+                String name = ten_sp.getText( ).toString( );
+                String price = gia_sp.getText( ).toString( );
+                String image = anh.getResources( ).toString( );
                 sanPham.setLoaiSanpham(String.valueOf(maLoaiSanPham));
                 sanPham.setNameSanpham(name);
                 sanPham.setPriceSanpham(Integer.parseInt(price));
                 sanPham.setImgSanpham(link);
-                Calendar calendar = Calendar.getInstance();
+                Calendar calendar = Calendar.getInstance( );
                 @SuppressLint("SimpleDateFormat") SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm dd/MM/yyyy");
-                String text = simpleDateFormat.format(calendar.getTime());
+                String text = simpleDateFormat.format(calendar.getTime( ));
                 sanPham.setCreateSanpham(text);
                 sanPham.setUpdatedSanpham(text);
                 if (sanPhamDAO.themSanpham(sanPham)) {
-                    Toast.makeText(getContext(), "Thêm thành công", Toast.LENGTH_SHORT).show();
-                    dialog.dismiss();
-                    list.clear();
-                    list.addAll(sanPhamDAO.getDSSanPham());
-                    phamAdapter.notifyDataSetChanged();
+                    Toast.makeText(getContext( ), "Thêm thành công", Toast.LENGTH_SHORT).show( );
+                    dialog.dismiss( );
+                    list.clear( );
+                    list.addAll(sanPhamDAO.getDSSanPham( ));
+                    phamAdapter.notifyDataSetChanged( );
                 } else {
-                    Toast.makeText(getContext(), "Thêm thất bại", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext( ), "Thêm thất bại", Toast.LENGTH_SHORT).show( );
                 }
             }
         });
@@ -199,19 +251,19 @@ public class SanPhamFragment extends Fragment {
     @Override
     public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getActivity().getMenuInflater();
+        MenuInflater inflater = getActivity( ).getMenuInflater( );
         inflater.inflate(R.menu.option_sua_xoa, menu);
 
     }
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
+        switch (item.getItemId( )) {
             case 0:
-                Toast.makeText(getContext(), "ban da xoa", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext( ), "ban da xoa", Toast.LENGTH_SHORT).show( );
                 return true;
             case 1:
-                Toast.makeText(getContext(), "ban da xoa", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext( ), "ban da xoa", Toast.LENGTH_SHORT).show( );
                 return true;
             default:
                 return super.onContextItemSelected(item);
@@ -221,10 +273,10 @@ public class SanPhamFragment extends Fragment {
 
     private void openImage() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+        if (intent.resolveActivity(getActivity( ).getPackageManager( )) != null) {
             activityResultLauncher.launch(intent);
         } else {
-            Toast.makeText(getContext(), "app ko ho tro action", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext( ), "app ko ho tro action", Toast.LENGTH_SHORT).show( );
         }
     }
 }
