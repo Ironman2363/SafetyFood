f1c7cee7ac1b6310e31ffe11813167e7c660b94b
hung90909 <hungnvph25605@fpt.edu.vn>
2022-12-15 11:37:53 +0700

    thu_vien
    thu_vien

--- app/src/main/java/com/example/safetyfood/Tab_Fragment/LoaiSanPhamFragment.java
+++ app/src/main/java/com/example/safetyfood/Tab_Fragment/LoaiSanPhamFragment.java
@@ -101,10 +101,10 @@ public class LoaiSanPhamFragment extends Fragment {
     }
 
 
-    public static Uri getImageUri(Context inContext, Bitmap inImage) {
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "IMG_" + Calendar.getInstance().getTime(),null);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
         return Uri.parse(path);
     }
 
